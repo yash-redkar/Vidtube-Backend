2a63a27ab487b89256bef555b3a2cd92090e1fd5
Yash Redkar <redkar19yash@gmail.com>
2025-10-10 22:56:56 +0530

    updated user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,6 +6,7 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
   try{
@@ -182,5 +183,54 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
+const logoutUser = asyncHandler( async (req,res) => {
+  await User.findByIdAndUpdate(
+    //TODO we need to comeback here after middleware 
+  )
+})
 
-export { registerUser,loginUser };
+const refreshAccessToken = asyncHandler(async (req,res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"Refresh token is required")
+  }
+
+  try {
+     const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+
+    if(!user){
+      throw new ApiError(401,"Invalid refresh token")
+    }
+
+    if(incomingRefreshToken != user?.refreshToken){
+      throw new ApiError(401,"Invalid refresh token")
+    }
+
+    const options ={
+      httpOnly: true,
+      secure: process.env.NODE_ENV == "production",
+    }
+
+    const {accessToken, refreshToken: newRefreshToken}= await generateAccessAndRefreshToken(user._id)
+
+    return res
+      .status(200)
+      .cookie("accessToken",accessToken, options)
+      .cookie("refreshToken",newRefreshToken,options)
+      .json(
+        new ApiResponse(200,{accessToken,refreshToken: newRefreshToken}, "Access token refreshed successfully")
+      );
+
+  } catch (error) {
+    throw new ApiError(401, "Something went wrong while refreshing access token")
+  }
+
+})
+
+
+export { registerUser,loginUser, refreshAccessToken};
